fa8aec4e9f753d2e47b4fec866656a79e9684bdf
Paolo-dev-it <paolo.nicoletti99@gmail.com>
2022-11-07 15:10:29 +0100

    forEach version completed on Snack 1

---
+++ main.js
@@ -0,0 +1,21 @@
+/* Snack 1
+Dato l'array di nomi:
+const myArray = ['Michele', 'Fabio', 'Roberto', 'Giovanni', 'Simone', 'Chiara'];
+e dati due numeri min e max (min più piccolo di max).
+Creiamo un nuovo array contenente i valori che hanno la posizione compresa tra i due numeri.
+Eseguiamo questo esercizio prima con forEach e poi con filter.
+esempio: dati i valori min: 2 e come max: 4 i nomi nell'array nuovo saranno i nomi compresi tra queste posizioni 2 e 4 */
+
+const myArray = ['Michele', 'Fabio', 'Roberto', 'Giovanni', 'Simone', 'Chiara'];
+
+const min = 2;
+
+const max = 4;
+
+myArray.forEach((element, index) => { //forEach version
+   
+    if (index >= min && index <= max){
+        console.log(element)
+    }
+
+});
